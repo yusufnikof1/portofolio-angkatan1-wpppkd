f116ee0f6f1e585bb2c88fa43cae1f3678be0f46
yusufnikof1 <yusufnikof@gmail.com>
2025-03-15 11:07:23 +0700

    First Commit

---
+++ inc/sidebar.php
@@ -0,0 +1,53 @@
+<aside id="sidebar" class="sidebar">
+
+    <ul class="sidebar-nav" id="sidebar-nav">
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="index.html">
+                <i class="bi bi-grid"></i>
+                <span>Dashboard</span>
+            </a>
+        </li><!-- End Dashboard Nav -->
+
+
+        <li class="nav-heading">Pages</li>
+
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="services.php">
+                <i class="bi bi-person"></i>
+                <span>Services</span>
+            </a>
+        </li><!-- End Profile Services -->
+
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="resume.php">
+                <i class="bi bi-person"></i>
+                <span>Resume</span>
+            </a>
+        </li><!-- End Profile Resume -->
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="contact.php">
+                <i class="bi bi-person"></i>
+                <span>Contact</span>
+            </a>
+        </li><!-- End Profile Contact -->
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="skill.php">
+                <i class="bi bi-person"></i>
+                <span>Skill</span>
+            </a>
+        </li><!-- End Profile Skill -->
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="setting.php">
+                <i class="bi bi-person"></i>
+                <span>Setting</span>
+            </a>
+        </li><!-- End Profile Setting -->
+    </ul>
+
+</aside>
---
+++ koneksi.php
@@ -0,0 +1,7 @@
+<?php
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$db = 'angkatan1_porto';
+
+$con = mysqli_connect($host, $user, $pass, $db);
